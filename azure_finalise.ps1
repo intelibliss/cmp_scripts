7b80027ecd568abbccbdbd6f8d31d9843a443cd0
habenani <57625218+habenani@users.noreply.github.com>
2020-02-04 21:06:43 +0000

    Create azure_finalise.ps1

---
+++ azure_finalise.ps1
@@ -0,0 +1,13 @@
+param(
+    [string]$version
+)
+# install AV
+$path = 'C:\av'
+$fileName = 'output.txt'
+$fullFilePath = (Join-Path -Path $path -ChildPath $fileName)
+New-Item -ItemType Directory -Path $path
+New-Item -ItemType File -Path $fullFilePath
+$version | Set-Content $fullFilePath
+
+# init data disks
+Get-Disk | Where-Object {$_.PartitionStyle -eq 'RAW'} | Initialize-Disk -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume
